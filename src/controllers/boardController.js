d14134849be9f230d13443149dbdadb4721aee95
MinhGiau <huynh.minhgiau.dev@gmail.com>
2026-04-24 04:59:42 +0700

    Error_Handling

--- src/controllers/boardController.js
+++ src/controllers/boardController.js
@@ -1,18 +1,15 @@
 import { StatusCodes } from 'http-status-codes'
+
 const createNew = async (req, res, next) => {
   try {
-    console.log(req.body)
+    // console.log(req.body)
 
 
     //control data to Service
 
     //if have the result we'll return Client
     res.status(StatusCodes.CREATED).json({ message: 'POST from Controller: API create new board' })
-  } catch (error) {
-    res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
-      errors: new Error(error).message
-    })
-  }
+  } catch (error) { next(error) }
 }
 
 export const boardController = {
