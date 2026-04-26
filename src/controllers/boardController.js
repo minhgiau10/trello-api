e5c1f230c0d678189dc701ddf93cba964831744a
MinhGiau <huynh.minhgiau.dev@gmail.com>
2026-04-26 03:32:09 +0000

    Create Model

--- src/controllers/boardController.js
+++ src/controllers/boardController.js
@@ -16,20 +16,4 @@ const createNew = async (req, res, next) => {
 
 export const boardController = {
   createNew
-import { StatusCodes } from 'http-status-codes'
-
-const createNew = async (req, res, next) => {
-  try {
-    // console.log(req.body)
-
-
-    //control data to Service
-
-    //if have the result we'll return Client
-    res.status(StatusCodes.CREATED).json({ message: 'POST from Controller: API create new board' })
-  } catch (error) { next(error) }
-}
-
-export const boardController = {
-  createNew
 }
--- src/services/boardService.js
+++ src/services/boardService.js
@@ -2,6 +2,7 @@ import { slugify } from '../utils/formatters.js'
 import ApiError from '~/utils/ApiError'
 
 const createNew = async (reqBody) => {
+  // eslint-disable-next-line no-useless-catch
   try {
 
     //treate logic data
@@ -12,8 +13,7 @@ const createNew = async (reqBody) => {
     // call to model to treat recordnewBoard in database
     // return result , in service always return
     return newBoard
-  } catch (error) {
-  }
+  } catch (error) { throw error }
 }
 
 export const boardService = {
