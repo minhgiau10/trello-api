5cdb25aec103ff85806604d7bf06a63c3d24dae4
MinhGiau <huynh.minhgiau.dev@gmail.com>
2026-04-26 01:31:01 +0000

    Service Layer

--- src/controllers/boardController.js
+++ src/controllers/boardController.js
@@ -1,4 +1,5 @@
 import { StatusCodes } from 'http-status-codes'
+import { boardService } from '~/services/boardService'
 
 const createNew = async (req, res, next) => {
   try {
@@ -6,9 +7,10 @@ const createNew = async (req, res, next) => {
 
 
     //control data to Service
+    const createdBoard = await boardService.createNew(req.body)
 
     //if have the result we'll return Client
-    res.status(StatusCodes.CREATED).json({ message: 'POST from Controller: API create new board' })
+    res.status(StatusCodes.CREATED).json(createdBoard)
   } catch (error) { next(error) }
 }
 
