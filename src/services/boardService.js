7ac423053a82e2533c16f28199fab20a0f981199
MinhGiau <huynh.minhgiau.dev@gmail.com>
2026-04-27 11:35:45 +0000

    API Create Board

--- src/services/boardService.js
+++ src/services/boardService.js
@@ -1,5 +1,5 @@
 import { slugify } from '../utils/formatters.js'
-import ApiError from '~/utils/ApiError'
+import { boardModel } from '../models/boardModel.js'
 
 const createNew = async (reqBody) => {
   // eslint-disable-next-line no-useless-catch
@@ -10,12 +10,17 @@ const createNew = async (reqBody) => {
       ...reqBody,
       slug: slugify(reqBody.title)
     }
-    // call to model to treat recordnewBoard in database
+    // call to model to treat record newBoard in database
+    const createdBoard = await boardModel.createNew(newBoard)
+
+    //Get record board after call
+    const getNewBoard = await boardModel.findOneById(createdBoard.insertedId)
+
     // return result , in service always return
-    return newBoard
+    return getNewBoard
   } catch (error) { throw error }
 }
 
 export const boardService = {
   createNew
-} 
+}
